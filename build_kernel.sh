f3830e4c1d8463c9468838cc8b7ced05b692f2c1
physwizz <pollockg86@gmail.com>
2025-03-04 16:34:12 +1100

    setup defconfig & gcc

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androidkernel-
+export CROSS_COMPILE=$(pwd)/toolchain/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9-lineage-19.1/bin/arm-linux-androidkernel-
 export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
 export CLANG_TRIPLE=arm-linux-gnueabi-
 export ARCH=arm
